710b3d2e60ed038f780ad3b45676e82973a9fa11
Muhamad Fajar <moch.fajardev@gmail.com>
2025-02-06 08:45:01 +0700

    Deploy Flutter web build Thu Feb  6 08:45:01 WIB 2025

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "963259181"
+    serviceWorkerVersion: "1485806643"
   }
 });
